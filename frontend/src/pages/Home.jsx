e2991e209b54ede335510c99917e2ef77aec878d
Amit Dahiya <113803002+Amitdahiya08@users.noreply.github.com>
2024-11-11 01:41:47 +0530

    Update api url

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
 
   const fetchProducts = async ()=>{
     try {
-        const url = "http://localhost:8080/products";
+        const url = "https://auth-app-api-eight.vercel.app/products";
         const headers = {
           headers :{
             "Authorization" : localStorage.getItem("token")
